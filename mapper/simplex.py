b7dd4f7e8c2f8619ccf5d6f4b8b9b86e563c5e21
Nick Dumas <drakonik@gmail.com>
2013-03-22 14:41:29 -0400

    Several changes, to be rebased.

--- mapper/simplex.py
+++ mapper/simplex.py
@@ -7,8 +7,27 @@ from collections import OrderedDict as od
 from pdb import set_trace
 
 #@profile_this
-def generate_map(size=None, seed=None, scale=None):
-    '''Generates a table of simplex values sampled across a two-dimensional plane.'''
+def generate_map(size=None, seed=None, scale=None, height=None):
+    '''Generates a table of simplex values for a two-dimensional plane.
+    Parameters
+    ----------
+    size : integer,optional
+        Dictates the world's dimensions.
+    seed : integer,optional
+        Seed for the simplex random number generator.
+    scale : float,optional
+        'Smooths' or 'roughens' the simplex function, creating greater or 
+        smaller variance in values at adjacent points. A fractional scale
+        argument increases the smoothness.
+    height : integer,optional
+        Dictates the total height of geographic features of the map. Very large
+        values will allow generation of maps with deep bodies of water or large
+        amounts of underground volume.
+    '''
+    if not height:
+        height = 15000
+    else:
+        height = height
     if not scale:
         scale = .000000000000000001
     if not size:
@@ -31,11 +50,17 @@ def generate_map(size=None, seed=None, scale=None):
             simplices[key].append(simp)
 
     #set_trace()
-    return df(simplices, index=ind)
+
+    noise = df(simplices, index=ind)
+    noise = (noise + 1) * height
+    return noise
+
+def coord_access(frame, coords=(0,0)):
+    x = 'X({})'.format(coords[0])
+    return frame[x][coords[1]]
 
 
 noise = generate_map(size=100, scale=.00001)
 
-noise = (noise + 1) * 500
 #print new_noise
 #print new_noise.describe()
