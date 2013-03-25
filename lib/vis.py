86d08aa257d9599187099773d41f6cec7108fc6f
Nick Dumas <drakonik@gmail.com>
2013-03-24 20:07:14 -0400

    More SCM screwups.

--- lib/vis.py
+++ lib/vis.py
@@ -2,24 +2,18 @@ from matplotlib import pyplot as PLT
 from matplotlib import cm as CM
 from matplotlib import mlab as ML
 import numpy as NP
+from lib.core import generate_map
 
-n = 1e5
-x = y = NP.linspace(-5, 5, 100)
-X, Y = NP.meshgrid(x, y)
-Z1 = ML.bivariate_normal(X, Y, 2, 2, 0, 0)
-Z2 = ML.bivariate_normal(X, Y, 4, 1, 1, 1)
-ZD = Z2 - Z1
-x = X.ravel()
-y = Y.ravel()
-z = ZD.ravel()
-gridsize=30
+gridsize=100
 PLT.subplot(111)
 
 # if 'bins=None', then color of each hexagon corresponds directly to its count
 # 'C' is optional--it maps values to x-y coordinates; if 'C' is None (default) then 
 # the result is a pure 2D histogram 
-
-PLT.hexbin(x, y, C=z, gridsize=gridsize, cmap=CM.jet, bins=None)
+size = 150
+noise = generate_map(size)
+x = y = NP.array(range(-size,size))
+PLT.hexbin(x, y, C=noise.values, gridsize=gridsize, cmap=CM.jet)
 PLT.axis([x.min(), x.max(), y.min(), y.max()])
 
 cb = PLT.colorbar()
