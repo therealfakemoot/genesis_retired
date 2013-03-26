a909188ca8c07c5efd788f28bd5882c2dbb36701
Nick Dumas <drakonik@gmail.com>
2013-03-26 18:08:23 -0400

    Added a topological contour graph to the visualization toolset.

--- lib/vis.py
+++ lib/vis.py
@@ -2,20 +2,25 @@ from matplotlib import pyplot as PLT
 from matplotlib import cm as CM
 from matplotlib import mlab as ML
 import numpy as NP
-from lib.core import generate_map
+from core import generate_map
+from matplotlib.pyplot import contour
 
-gridsize=100
-PLT.subplot(111)
+def histo():
+    gridsize=100
+    PLT.subplot(111)
 
 # if 'bins=None', then color of each hexagon corresponds directly to its count
 # 'C' is optional--it maps values to x-y coordinates; if 'C' is None (default) then 
 # the result is a pure 2D histogram 
-size = 150
-noise = generate_map(size)
-x = y = NP.array(range(-size,size))
-PLT.hexbin(x, y, C=noise.values, gridsize=gridsize, cmap=CM.jet)
-PLT.axis([x.min(), x.max(), y.min(), y.max()])
+    size = 150
+    noise = generate_map(size)
+    x = y = NP.array(range(-size,size))
+    PLT.hexbin(x, y, C=noise.values, gridsize=gridsize, cmap=CM.jet)
+    PLT.axis([x.min(), x.max(), y.min(), y.max()])
 
-cb = PLT.colorbar()
-cb.set_label('mean value')
-PLT.show()
+    cb = PLT.colorbar()
+    cb.set_label('mean value')
+    PLT.show()
+
+def topo(noisemap):
+    return contour(noisemap)
