6c0e280f6715c5b3f887015d9da87e10cdd69112
Nick Dumas <drakonik@gmail.com>
2013-04-10 13:53:37 -0400

    Now test_vis makes sure that chunked DataFrames have the right shape.

--- tests/test_vis.py
+++ tests/test_vis.py
@@ -1,7 +1,12 @@
 from genesis.core.genesis import generate_map
 from genesis.core.vis import chunk
 
-def test_chunk():
+def test_chunk_len():
     noise = generate_map(100) 
     chunks = [c for c in chunk(noise, (0,0, 10, 10))]
     assert len(chunks) == 100
+
+def test_chunk_shape():
+    noise = generate_map(100) 
+    c = chunk(noise, (0,0, 10, 10)).next()
+    assert c.shape == (10,10)
