bd9c9ec66fb61ccc149b753f9fec5a0ef6379e0a
Nick Dumas <drakonik@gmail.com>
2013-04-12 12:44:56 -0400

    Tests were running slowly.
    I halved the test suite's map size because one and a half seconds for the test suite was too slow.

--- tests/test_vis.py
+++ tests/test_vis.py
@@ -2,11 +2,11 @@ from genesis.core.genesis import generate_map
 from genesis.core.vis import chunk
 
 def test_chunk_len():
-    noise = generate_map(100) 
+    noise = generate_map(50) 
     chunks = [c for c in chunk(noise, (0,0, 10, 10))]
-    assert len(chunks) == 100
+    assert len(chunks) == 25
 
 def test_chunk_shape():
-    noise = generate_map(100) 
+    noise = generate_map(50) 
     c = chunk(noise, (0,0, 10, 10)).next()
     assert c.shape == (10,10)
