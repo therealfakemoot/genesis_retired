c806c198f5359d3012e1050c3f681f5b0c880865
Nick Dumas <drakonik@gmail.com>
2013-04-13 17:28:20 -0400

    Updated command line arguments for genesis.py.
    I added a help message to all the arguments and used argparse's argument groups to make things more sensible.

--- genesis.py
+++ genesis.py
@@ -8,17 +8,18 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--nosave',help='Prevents Genesis from serializing the generated map to disk.', action='store_true')
+map_opts = parser.add_argument_group('Map Properties', 'These arguments relate to the attributes of map generation.')
 
-parser.add_argument('-l','--load', action='store_false')
+map_opts.add_argument('--size', help='Desired size of the generated world in terms of pixels.')
+map_opts.add_argument('--scale', help='A float that determines the \'smoothness\' of the noisemap. Smaller values create more smoothness.')
+map_opts.add_argument('--height', help='Maximum height of the topological map. Units are arbitrary; this value is up for interpretation by the end user.')
 
-parser.add_argument('--size')
-parser.add_argument('--scale')
-parser.add_argument('--height')
+save_opts = parser.add_argument_group('Save Options', 'These arguments dictate when, where, and if data is saved to or read from the disk.')
+save_opts.add_argument('--nosave',help='Prevents Genesis from serializing the generated map to disk.', action='store_true')
+save_opts.add_argument('-l','--load', action='store_false', dest='load', help='Path to a previously serialised noisemap.')
+
 
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    if args.demo:
-        demo.demo()
