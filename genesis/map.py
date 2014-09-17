05c9427dc870bcd2ba03915f93eb363147fc48d4
Nick Dumas <drakonik@gmail.com>
2014-09-17 15:37:51 -0400

    remove comment.

--- genesis/map.py
+++ genesis/map.py
@@ -6,7 +6,6 @@ import noise
 
 def chunk(iterable, n, fillvalue=None):
     "Collect data into fixed-length chunks or blocks"
-    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
     args = [iter(iterable)] * n
     return izip_longest(fillvalue=fillvalue, *args)
 
