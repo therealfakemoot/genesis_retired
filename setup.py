aff165ad9f0c60a60946d64637ec9f230128365e
Nick Dumas <drakonik@gmail.com>
2014-09-17 15:20:39 -0400

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='genesis',
+    description='Procedural World Generation',
+    author='Nick Dumas',
+    author_email='drakonik@gmail.com',
+    url='https://github.com/therealfakemoot/Genesis/',
+    packages=find_packages(),
+    zip_safe=False
+)
