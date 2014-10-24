df389291eaeb92f762a371ba5c38686e4e483522
Nick Dumas <drakonik@gmail.com>
2014-10-24 17:08:52 -0400

    Remove genesis.vis and merge it into genesis.map.
    With the actual plotting of maps being redone entirely, now is the time
    to fold functionality together and condense what can be.

--- genesis/map.py
+++ genesis/map.py
@@ -1,7 +1,39 @@
 from itertools import izip_longest, product
+from functools import partial
 
 import numpy as np
 import noise
+
+def view(frame, viewport):
+    '''
+    Returns a windowed version of the dataframe.
+    viewport: tuple
+        A tuple of the form (x,y,height,width)
+    '''
+
+    x, y, h, w = viewport
+    return frame.ix[x:x + w, y:y + h]
+
+
+def chunk(frame, viewport):
+    '''
+    Slices a DataFrame into smaller, equal-sized DataFrames according to viewport.
+    viewport: tuple
+        A tuple of the form (x,y,height,width)
+    '''
+
+    xmax, ymax = frame.shape
+    x, y, h, w = viewport
+    if any(n % 2 != 0 for n in viewport):
+        raise ValueError('Viewport values must be even integers.')
+    if h != w:
+        raise ValueError('Viewport must be of equal dimensions.')
+    chunks = xmax / w
+    fview = partial(view, frame)
+    for i in range(chunks):
+        for j in range(chunks):
+            yield fview((y + h * j, x + w * i, h - 1, w - 1))
+
 
 
 def chunk(iterable, n, fillvalue=None):
