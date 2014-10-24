ca3bd4c1e697c1540ee80b0ad98986a37df618fa
Nick Dumas <drakonik@gmail.com>
2014-10-24 16:15:12 -0400

    Add docstring for map_noise.

--- genesis/map.py
+++ genesis/map.py
@@ -11,6 +11,20 @@ def chunk(iterable, n, fillvalue=None):
 
 
 def map_noise(height, width, z, scale=.001):
+    '''Creates a sequence of values representing simplex noise.
+    Parameters
+    ----------
+
+    height : integer
+        Assuming the origin is in the lower left corner, height controls
+        the maximum value of the y axis of the map.
+    width : integer
+        Controls the maximum value of the x axis, as per the `height`
+        parameter.
+    z : integer
+        The z parameter scales the 'height' or z-axis of the map when considered
+        in three dimensions, with height and width providing x and y.
+    '''
     coords = product(range(height), range(width))
     for x, y in coords:
         yield noise.snoise3(x * scale, y * scale, 0) * z
