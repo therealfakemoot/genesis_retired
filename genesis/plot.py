d9a1b4f17677b13a7d0484855c0a110a8747d4ee
Nick Dumas <drakonik@gmail.com>
2014-10-24 17:37:43 -0400

    Add genesis.plot.
    This will be the module responsible for creating matplotlib plots of
    maps. Currently has a working example in test().

---
+++ genesis/plot.py
@@ -0,0 +1,30 @@
+import map
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_map(noisemap):
+    matplotlib.rcParams['xtick.direction'] = 'out'
+    matplotlib.rcParams['ytick.direction'] = 'out'
+
+    x = np.arange(0, noisemap.shape[0])
+    y = np.arange(0, noisemap.shape[1])
+    X, Y = np.meshgrid(x, y)
+    #difference of Gaussians
+
+    # Create a simple contour plot with labels using default colors.  The
+    # inline argument to clabel will control whether the labels are draw
+    # over the line segments of the contour, removing the lines beneath
+    # the label
+    plt.figure()
+    cs = plt.contour(X, Y, noisemap._array)
+    plt.clabel(cs, inline=1, fontsize=10)
+    plt.title('Simplest default with labels')
+
+    plt.show()
+
+
+def test(x, y):
+    m = map.Map((x, y))
+    plot_map(m)
