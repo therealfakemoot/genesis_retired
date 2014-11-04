9ff795c974eee2d24f9c73c8eda4d50e2cf7b445
Nick Dumas <drakonik@gmail.com>
2014-11-04 14:09:10 -0500

    Remove irrelevant comments.
    These comments have been hanging around from when I copied this code
    from the matplotlib contour plot demo. No longer relevant to the
    function they're in.

--- genesis/plot.py
+++ genesis/plot.py
@@ -16,12 +16,7 @@ def plot_map(noisemap):
     x = np.arange(0, noisemap.shape[0])
     y = np.arange(0, noisemap.shape[1])
     X, Y = np.meshgrid(x, y)
-    #difference of Gaussians
 
-    # Create a simple contour plot with labels using default colors.  The
-    # inline argument to clabel will control whether the labels are draw
-    # over the line segments of the contour, removing the lines beneath
-    # the label
     plt.figure()
     cs = plt.contour(X, Y, noisemap._array)
     plt.clabel(cs, inline=1, fontsize=10)
